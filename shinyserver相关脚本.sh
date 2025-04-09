d8cd1f1b4b0f77f4459b8ee2e53fcefc7b4916c7
Dr. Wang <37176118+wangyong950511@users.noreply.github.com>
2025-04-09 17:07:27 +0800

    Update shinyserver相关脚本.sh

--- shinyserver相关脚本.sh
+++ shinyserver相关脚本.sh
@@ -1,6 +1,9 @@
-
-
 # 更新脚本到服务器
 sudo cp ui.R server.R /srv/shiny-server/myapp/
 
+# 查看日志
+ls -lt /var/log/shiny-server/myapp-*.log | head -n 1
+tail -f /var/log/shiny-server/myapp-shiny-20250409-135153-39289.log
 
+# 重启shiny-server
+sudo systemctl restart shiny-server
