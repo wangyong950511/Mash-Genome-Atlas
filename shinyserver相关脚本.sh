91d1e6e33d687cff2ddd6c5eb6ea324c97e7eb36
Dr. Wang <37176118+wangyong950511@users.noreply.github.com>
2025-05-07 08:39:59 +0800

    Update shinyserver相关脚本.sh

--- shinyserver相关脚本.sh
+++ shinyserver相关脚本.sh
@@ -1,13 +1,13 @@
+## 日志服务
 # 更新脚本到服务器
 sudo cp ui.R server.R global.R /srv/shiny-server/myapp/
-
 # 查看日志
 tail -f $(ls -t /var/log/shiny-server/myapp-*.log | head -n 1)
-
 # 重启shiny-server
 sudo systemctl restart shiny-server
 
-## 网站维护
+
+## 安全维护
 # 防火墙状态
 sudo tail -n 100 /var/log/nginx/access.log
 #查看封禁IP
